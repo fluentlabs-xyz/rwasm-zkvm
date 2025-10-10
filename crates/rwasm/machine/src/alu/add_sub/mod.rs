152a2f0801572f75ee2753b2050e9641c56489d1
Suleyman Kardas <suleyman@fluentlabs.xyz>
2025-10-10 17:26:20 +0300

    test_malicious_add_sub is added.

--- crates/rwasm/machine/src/alu/add_sub/mod.rs
+++ crates/rwasm/machine/src/alu/add_sub/mod.rs
@@ -289,15 +289,15 @@ mod tests {
         events::AluEvent,
         ExecutionRecord, DEFAULT_PC_INC,
     };
-    use sp1_stark::{
-        air::MachineAir, baby_bear_poseidon2::BabyBearPoseidon2,
-        MachineProver, StarkGenericConfig,
-    };
+    use sp1_stark::{air::MachineAir, baby_bear_poseidon2::BabyBearPoseidon2, chip_name, CpuProver, MachineProver, StarkGenericConfig, Val};
     use std::sync::LazyLock;
 
     use core::borrow::Borrow;
-     use super::*;
-    use crate::utils::{uni_stark_prove as prove, uni_stark_verify as verify};
+    use rwasm_executor::events::MemoryRecordEnum;
+    use crate::io::SP1Stdin;
+    use crate::rwasm::RwasmAir;
+    use super::*;
+    use crate::utils::{run_malicious_test, uni_stark_prove as prove, uni_stark_verify as verify};
 
     /// Lazily initialized record for use across multiple tests.
     /// Consists of random `ADD` and `SUB` instructions.
@@ -511,5 +511,92 @@ mod tests {
 
         // Convert the trace to a row major matrix.
         RowMajorMatrix::new(rows.into_iter().flatten().collect::<Vec<_>>(), NUM_ADD_SUB_COLS)
+    }
+
+    #[test]
+    fn test_malicious_add_sub() {
+        const NUM_TESTS: usize = 5;
+
+        for opcode in [Opcode::I32Add, Opcode::I32Sub] {
+            for _ in 0..NUM_TESTS {
+                let op_a = thread_rng().gen_range(0..u32::MAX);
+                let op_b = thread_rng().gen_range(0..u32::MAX);
+                let op_c = thread_rng().gen_range(0..u32::MAX);
+
+                let correct_op_a = if opcode == Opcode::I32Add {
+                    op_b.wrapping_add(op_c)
+                } else {
+                    op_b.wrapping_sub(op_c)
+                };
+
+                assert_ne!(op_a, correct_op_a);
+                let instructions = vec![
+                    Opcode::I32Const(op_b.into()),
+                    Opcode::I32Const(op_c.into()),
+                    Opcode::I32Const(5.into()),
+                    Opcode::I32Const(10.into()),
+                    opcode,
+                    Opcode::I32Add,
+                ];
+                let program = Program::from_instrs(instructions);
+                let stdin = SP1Stdin::new();
+
+                type P = CpuProver<BabyBearPoseidon2, RwasmAir<BabyBear>>;
+
+                let malicious_trace_pv_generator = move |prover: &P,
+                                                         record: &mut ExecutionRecord|
+                                                         -> Vec<(
+                                                             String,
+                                                             RowMajorMatrix<Val<BabyBearPoseidon2>>,
+                                                         )> {
+                    let mut malicious_record = record.clone();
+                    // The ALU operation is the 5th instruction, so we modify the 5th cpu event.
+                    if malicious_record.cpu_events.len() > 4 {
+                        malicious_record.cpu_events[4].res = op_a;
+                    }
+                    if opcode == Opcode::I32Add {
+                        if malicious_record.add_events.len() > 0 {
+                            malicious_record.add_events[0].a = op_a;
+                        }
+                    } else if opcode == Opcode::I32Sub {
+                        if malicious_record.sub_events.len() > 0 {
+                            malicious_record.sub_events[0].a = op_a;
+                        }
+                    } else {
+                        unreachable!()
+                    }
+                    let mut traces = prover.generate_traces(&malicious_record);
+
+                    let add_sub_chip_name = chip_name!(AddSubChip, BabyBear);
+                    for (chip_name, trace) in traces.iter_mut() {
+                        if *chip_name == add_sub_chip_name {
+                            // The add instructions are added first to the trace, before the sub instructions.
+                            // When the opcode is `I32Sub`, the `I32Add` instruction runs first, so the
+                            // sub event is at index 1.
+                            let index = if opcode == Opcode::I32Add { 0 } else { 1 };
+
+                            let first_row = trace.row_mut(index);
+                            let first_row: &mut AddSubCols<BabyBear> = first_row.borrow_mut();
+                            if opcode == Opcode::I32Add {
+                                first_row.add_operation.value = op_a.into();
+                            } else {
+                                first_row.add_operation.value = op_b.into();
+                            }
+                        }
+                    }
+
+                    traces
+                };
+
+                let result =
+                    run_malicious_test::<P>(program, stdin, Box::new(malicious_trace_pv_generator));
+                println!("Result for {:?}: {:?}", opcode, result);
+                let add_sub_chip_name = chip_name!(AddSubChip, BabyBear);
+                assert!(
+                    result.is_err()
+                        && result.unwrap_err().is_constraints_failing(&add_sub_chip_name)
+                );
+            }
+        }
     }
 }
