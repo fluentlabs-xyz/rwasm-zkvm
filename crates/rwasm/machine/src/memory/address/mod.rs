ce75f581ca1ae6e40c5ed3efcf35cca9696af756
Aliaksei Dziadziuk <adziadziuk@irreducible.com>
2025-10-20 11:04:14 +0200

    subtract the SP_END to work with the address that is in the range [0..SP_START - SP_END]

--- crates/rwasm/machine/src/memory/address/mod.rs
+++ crates/rwasm/machine/src/memory/address/mod.rs
@@ -7,16 +7,15 @@ use sp1_derive::AlignedBorrow;
 use rwasm_executor::{ByteOpcode, SP_START};
 use sp1_stark::air::SP1AirBuilder;
 
-const STACK_END_LOW_8BITS: u8 = (SP_END & 0xFF) as u8;
-const STACK_UB_LOW_8BITS: u8 = ((SP_START + UNIT) & 0xFF) as u8;
-const STACK_UB_HI_8BITS: u8 = (((SP_START + UNIT) & 0xFF00) >> 8) as u8;
+const STACK_UB_LOW_8BITS_WITH_OFFSET: u8 = ((SP_START - SP_END + UNIT) & 0xFF) as u8;
+const STACK_UB_HI_8BITS_WITH_OFFSET: u8 = (((SP_START - SP_END + UNIT) & 0xFF00) >> 8) as u8;
 /// Memory read-write access.
 
 #[derive(AlignedBorrow, Default, Debug, Clone, Copy)]
 #[repr(C)]
 pub struct StackAddressCols<T> {
-    pub addr_hi_8bits: T,
-    pub addr_low_8bits: T,
+    addr_hi_8bits: T,
+    addr_low_8bits: T,
     pub addr_hi_is_zero: T,
     pub addr_hi_is_not_edge: T,
     pub addr_hi_is_eq_stack_ub_hi: T,
@@ -25,14 +24,20 @@ pub struct StackAddressCols<T> {
 impl<F: PrimeField32> StackAddressCols<F> {
     pub fn populate(&mut self, addr: u32, output: &mut impl ByteRecord) {
         assert_ne!(addr, 0);
-        let addr: u16 = addr.try_into().unwrap();
+        // We subtract the SP_END to work with the address that is in the range [0..SP_START -
+        // SP_END]
+        let (addr_with_offset, overflow) = addr.overflowing_sub(SP_END);
 
-        let addr_hi_8bits: u8 = ((addr & 0xFF00) >> 8).try_into().unwrap();
-        let addr_low_8bits: u8 = (addr & 0xFF).try_into().unwrap();
+        assert!(!overflow);
+
+        let addr_with_offset: u16 = addr_with_offset.try_into().unwrap();
+
+        let addr_hi_8bits: u8 = ((addr_with_offset & 0xFF00) >> 8) as u8;
+        let addr_low_8bits: u8 = (addr_with_offset & 0xFF) as u8;
         self.addr_hi_8bits = F::from_canonical_u8(addr_hi_8bits);
         self.addr_low_8bits = F::from_canonical_u8(addr_low_8bits);
 
-        let addr_hi_is_eq_stack_ub_hi = addr_hi_8bits == STACK_UB_HI_8BITS;
+        let addr_hi_is_eq_stack_ub_hi = addr_hi_8bits == STACK_UB_HI_8BITS_WITH_OFFSET;
         self.addr_hi_is_eq_stack_ub_hi = F::from_bool(addr_hi_is_eq_stack_ub_hi);
 
         let addr_hi_is_zero = addr_hi_8bits == 0;
@@ -49,17 +54,7 @@ impl<F: PrimeField32> StackAddressCols<F> {
                 a1: true as u16,
                 a2: 0,
                 b: addr_hi_8bits,
-                c: STACK_UB_HI_8BITS,
-            });
-        }
-
-        if addr_hi_is_zero {
-            output.add_byte_lookup_event(ByteLookupEvent {
-                opcode: ByteOpcode::LTU,
-                a1: true as u16,
-                a2: 0,
-                b: STACK_END_LOW_8BITS,
-                c: addr_low_8bits,
+                c: STACK_UB_HI_8BITS_WITH_OFFSET,
             });
         }
 
@@ -69,7 +64,7 @@ impl<F: PrimeField32> StackAddressCols<F> {
                 a1: true as u16,
                 a2: 0,
                 b: addr_low_8bits,
-                c: STACK_UB_LOW_8BITS,
+                c: STACK_UB_LOW_8BITS_WITH_OFFSET,
             });
         }
     }
@@ -81,7 +76,7 @@ impl<T: Copy> StackAddressCols<T> {
     {
         let hi = self.addr_hi_8bits.into();
         let low = self.addr_low_8bits.into();
-        hi * AB::Expr::from_canonical_u32(1 << 8) + low
+        hi * AB::Expr::from_canonical_u32(1 << 8) + low + AB::Expr::from_canonical_u32(SP_END)
     }
 
     pub fn range_check<AB: SP1AirBuilder>(builder: &mut AB, cols: StackAddressCols<AB::Var>) {
@@ -100,13 +95,12 @@ impl<T: Copy> StackAddressCols<T> {
         );
 
         // check edge cases of addr_hi_8bits
-
         builder.when(is_real.clone()).when(cols.addr_hi_is_zero).assert_zero(cols.addr_hi_8bits);
 
-        builder
-            .when(is_real)
-            .when(cols.addr_hi_is_eq_stack_ub_hi)
-            .assert_eq(cols.addr_hi_8bits, AB::Expr::from_canonical_u8(STACK_UB_HI_8BITS));
+        builder.when(is_real).when(cols.addr_hi_is_eq_stack_ub_hi).assert_eq(
+            cols.addr_hi_8bits,
+            AB::Expr::from_canonical_u8(STACK_UB_HI_8BITS_WITH_OFFSET),
+        );
 
         // If it's not an edge case, we check that addr_hi_8bits is located within the space between
         // the edges
@@ -114,25 +108,16 @@ impl<T: Copy> StackAddressCols<T> {
             AB::Expr::from_canonical_u32(ByteOpcode::LTU as u32),
             AB::Expr::from_bool(true),
             cols.addr_hi_8bits,
-            AB::Expr::from_canonical_u8(STACK_UB_HI_8BITS),
+            AB::Expr::from_canonical_u8(STACK_UB_HI_8BITS_WITH_OFFSET),
             cols.addr_hi_is_not_edge,
         );
 
         // Check addr_low_8bits in case addr_hi_8bits is an edge case
-
-        builder.send_byte(
-            AB::Expr::from_canonical_u32(ByteOpcode::LTU as u32),
-            AB::Expr::from_bool(true),
-            AB::Expr::from_canonical_u8(STACK_END_LOW_8BITS),
-            cols.addr_low_8bits,
-            cols.addr_hi_is_zero,
-        );
-
         builder.send_byte(
             AB::Expr::from_canonical_u32(ByteOpcode::LTU as u32),
             AB::Expr::from_bool(true),
             cols.addr_low_8bits,
-            AB::Expr::from_canonical_u8(STACK_UB_LOW_8BITS),
+            AB::Expr::from_canonical_u8(STACK_UB_LOW_8BITS_WITH_OFFSET),
             cols.addr_hi_is_eq_stack_ub_hi,
         );
     }
