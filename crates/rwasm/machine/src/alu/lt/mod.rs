97e3ebd5b2ae8a68169ce80796cab9e8e6ce87af
Suleyman Kardas <suleyman@fluentlabs.xyz>
2025-10-23 13:33:43 +0300

    reduce number of event for I32Gts, I32Lts,I32GtU, I32LtU

--- crates/rwasm/machine/src/alu/lt/mod.rs
+++ crates/rwasm/machine/src/alu/lt/mod.rs
@@ -571,18 +571,18 @@ mod tests {
 
         prove_babybear_template(&mut shard);
     }
+
     #[test]
-    fn test_malicious_ltu() {
-        run_malicious_lt(Opcode::I32LtU)
-    }
-    #[test]
-    fn test_malicious_lts() {
-        run_malicious_lt(Opcode::I32LtS)
+    fn test_malicious_lt() {
+        for opcode in [Opcode::I32Eq, Opcode::I32LtS,Opcode::I32GtS,Opcode::I32LtU,Opcode::I32GtU,Opcode::I32LeS,Opcode::I32GeS,Opcode::I32LeU,Opcode::I32GeU] {
+            run_malicious_lt(opcode)
+        }
+
     }
 
     fn run_malicious_lt(opcode: Opcode) {
         use core::borrow::BorrowMut;
-        const NUM_TESTS: usize = 10;
+        const NUM_TESTS: usize = 2;
 
         let rng = thread_rng();
         for _ in 0..NUM_TESTS {
@@ -590,7 +590,16 @@ mod tests {
             let op_c = thread_rng().gen_range(0..u32::MAX);
 
             let correct_op_a =
-                if opcode == Opcode::I32LtU { op_b < op_c } else { (op_b as i32) < (op_c as i32) };
+                if opcode == Opcode::I32LtU { op_b < op_c }
+                else if opcode == Opcode::I32GtU { op_b > op_c }
+                else if opcode == Opcode::I32LeU { op_b <= op_c }
+                else if opcode == Opcode::I32GeU { op_b >= op_c }
+                else if opcode == Opcode::I32Eq { op_b == op_c }
+                else if opcode == Opcode::I32LtS { (op_b as i32) < (op_c as i32) }
+                else if opcode == Opcode::I32GtS { (op_b as i32) > (op_c as i32) }
+                else if opcode == Opcode::I32LeS { (op_b as i32) <= (op_c as i32) }
+                else if opcode == Opcode::I32GeS { (op_b as i32) >= (op_c as i32) }
+                else { true };
             //Pops rhs, then lhs, pushes i32( lhs <_s rhs ? 1 : 0 )
             let op_a = !correct_op_a;
             let program = Program::from_instrs(vec![
@@ -628,6 +637,7 @@ mod tests {
             let result =
                 run_malicious_test::<P>(program, stdin, Box::new(malicious_trace_pv_generator));
             let chip_name = chip_name!(CpuChip, BabyBear);
+            println!("run_malicious_lt for opcode : {:?}", opcode);
             assert!(result.is_err());
             assert!(result.unwrap_err().is_constraints_failing(&chip_name));
         }
