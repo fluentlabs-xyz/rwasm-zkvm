dfaa10ddca4c2b6e42ca19456b83eb760dfbc1c7
Suleyman Kardas <suleyman@fluentlabs.xyz>
2025-11-05 04:46:24 +0300

    update stats for popcnt

--- crates/rwasm/executor/src/record.rs
+++ crates/rwasm/executor/src/record.rs
@@ -276,6 +276,7 @@ impl MachineRecord for ExecutionRecord {
         stats.insert("shift_right_events".to_string(), self.shift_right_events.len());
         stats.insert("divrem_events".to_string(), self.divrem_events.len());
         stats.insert("lt_events".to_string(), self.lt_events.len());
+        stats.insert("popcnt_events".to_string(), self.popcnt_events.len());
         stats.insert("trailing_events".to_string(), self.trailing_events.len());
         stats.insert("memory_opcodes_events".to_string(), self.memory_instr_events.len());
         stats.insert("branch_events".to_string(), self.branch_events.len());
@@ -313,6 +314,7 @@ impl MachineRecord for ExecutionRecord {
         self.shift_right_events.append(&mut other.shift_right_events);
         self.divrem_events.append(&mut other.divrem_events);
         self.lt_events.append(&mut other.lt_events);
+        self.popcnt_events.append(&mut other.popcnt_events);
         self.trailing_events.append(&mut other.trailing_events);
         self.memory_instr_events.append(&mut other.memory_instr_events);
         self.branch_events.append(&mut other.branch_events);
