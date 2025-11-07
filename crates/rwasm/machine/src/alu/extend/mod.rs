d4a92d2604db8506089a6f8b7e14e40f5566a139
Suleyman Kardas <suleyman@fluentlabs.xyz>
2025-11-07 18:41:39 +0300

    remove redundant constraint

--- crates/rwasm/machine/src/alu/extend/mod.rs
+++ crates/rwasm/machine/src/alu/extend/mod.rs
@@ -118,9 +118,6 @@ where
         builder.when(local.is_extend16s).assert_eq(local.a[2], local.msb * ff.clone());
         builder.when(local.is_extend16s).assert_eq(local.a[3], local.msb * ff.clone());
 
-        // Ensure that `msb` is zero when no extend operation is active.
-        builder.assert_zero(local.msb * (is_real.clone() - AB::Expr::one()));
-
         builder.receive_instruction(
             AB::Expr::zero(),
             AB::Expr::zero(),
