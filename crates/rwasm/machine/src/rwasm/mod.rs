b142d7c709e87e2f40a35433345a79725d19bc0f
Suleyman Kardas <suleyman@fluentlabs.xyz>
2025-11-18 01:52:35 +0300

    refactor code

--- crates/rwasm/machine/src/rwasm/mod.rs
+++ crates/rwasm/machine/src/rwasm/mod.rs
@@ -30,8 +30,8 @@ use crate::{
 pub(crate) mod rwasm_chips {
     pub use crate::{
         alu::{
-            AddSubChip, BitwiseChip, DivRemChip, ExtendChip, LtChip, MulChip, RotateChip,
-            ShiftLeft, ShiftRightChip,Mul64Chip,
+            AddSubChip, BitwiseChip, DivRemChip, ExtendChip, LtChip, Mul64Chip, MulChip,
+            RotateChip, ShiftLeft, ShiftRightChip,
         },
         bytes::ByteChip,
         cpu::CpuChip,
